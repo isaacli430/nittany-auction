3587378258b31190c2c3d52786d3ae3c5fdbd6da
isaacli430 <isaacli430@gmail.com>
2026-03-23 01:50:24 -0400

    did some work to login

--- app.py
+++ app.py
@@ -5,9 +5,12 @@ from flask import Flask, render_template, request
 import random
 import sqlite3 as sql
 
-app = Flask(__name__)
+import secrets
 
+app = Flask(__name__)
 host = 'http://127.0.0.1:5000/'
+
+app.secret_key = secrets.token_bytes()
 
 def load_db():
     with open("create_db.sql") as f:
@@ -32,8 +35,23 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/login')
+@app.route('/login', methods = ['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        connection = sql.connect("database.db")
+        cursor = connection.cursor()
+
+        cursor.execute("SELECT * FROM Users WHERE email = ? AND password = ?;", (email, password))
+        results = cursor.fetchall()
+        connection.close()
+
+        if len(results) == 0:
+            return render_template('login.html', fail = True)
+        
+
     return render_template('login.html')
 
 if __name__ == "__main__":
