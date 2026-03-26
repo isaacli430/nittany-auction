5fb8dbc70c702a21cadc6acba4a76b0444f2f42b
isaacli430 <isaacli430@gmail.com>
2026-03-26 00:48:18 -0400

    add error page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import Root from "./routes/root";
 import Login from "./routes/login";
 import Logout from "./routes/logout";
 import Products from './routes/products';
+import NotFound from './routes/notfound';
 
 const App = () => {
+
     return (
         <BrowserRouter>
             {/* Routes */}
@@ -14,6 +16,9 @@ const App = () => {
                 <Route path="/login" element={<Login />} />
                 <Route path="/logout" element={<Logout />} />
                 <Route path="/products" element={<Products />} />
+                <Route path="/not-found" element={<NotFound />} />
+
+                <Route path="*" element={<Navigate to="/not-found" />} />
             </Routes>
         </BrowserRouter>
     );
