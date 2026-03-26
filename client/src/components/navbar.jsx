b352960c0ffc1d95057d5d424d2b48e10c3e82fb
isaacli430 <isaacli430@gmail.com>
2026-03-26 15:20:51 -0400

    change navbar style

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,18 +1,42 @@
 import { Link } from 'react-router-dom';
 
+import { CgProfile } from "react-icons/cg";
+
+import { useState } from 'react';
+
 
 const Navbar = ({ logged }) => {
 
+    const [profileMenu, setProfileMenu] = useState(false);
+
+    const toggleMenu = () => {
+        if (profileMenu) {
+            setProfileMenu(false);
+        } else {
+            setProfileMenu(true);
+        }
+    }
+
     return (
-        <nav>
-            <Link to="/">Home</Link> |{" "}
-            <Link to="/products">Products</Link> |{" "}
-            {logged ? (
-                <Link to="/logout">Logout</Link>
-            ) : (
-                <Link to="/login">Login</Link>
-            )}
-        </nav>
+        <>
+            <nav className='flex pr-4 pl-4 pt-0 pb-0 bg-slate-300'>
+                <Link className='nav-link' to="/">Nittany Auction</Link>
+                <Link className='nav-link' to="/products">Products</Link>
+                <button className='nav-link ml-auto! pr-4! pl-4! hover:cursor-pointer' onClick={toggleMenu}><CgProfile /></button>
+            </nav>
+            {profileMenu &&
+                    <div className='z-10 absolute bg-slate-600 right-3 rounded-sm pr-0 pl-0 pt-4 pb-4 shadow-lg'>
+                        {logged ? (
+                            <>
+                                <Link className='profile-link' to="/">View Profile</Link>
+                                <Link className='profile-link' to="/logout">Logout</Link>
+                            </>
+                        ) : (
+                            <Link className='profile-link' to="/login">Login</Link>
+                        )}
+                    </div>
+                }
+        </>
     )
 }
 
