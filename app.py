d813e0d3325e847000beec2c31f6878256a21800
ljq5040 <ljq5040@psu.edu>
2026-04-19 20:07:33 -0400

    Update Settings page with getting information, add&delete cards

--- app.py
+++ app.py
@@ -186,6 +186,166 @@ def register():
     connection.close()
     return json.dumps({"message": "Account created successfully."})
 
+# ================================
+# Setting page
+# ================================
+def mask_card(card):
+    number = str(card)
+    if len(number) >= 4:
+        return "****-****-****-" + number[-4:]
+    return number
+
+def mask_cvv(cvv):
+    return "***"
+
+@app.route('/api/settings',methods = ['GET'])
+#Getting all Setting page information
+def get_settings():
+    token = request.headers.get('Authorization')
+    if not token:
+        return Response(json.dumps({"error": "No token"}),status = 400)
+    connect = sql.connect("database.db")
+    cursor = connect.cursor()
+    cursor.execute("SELECT email FROM Tokens WHERE = ?", (token,))
+    user = cursor.fetchone()
+    if not user:
+        connect.close()
+        return Response(json.dumps({"error": "Invalid session"}),status = 400)
+
+    email = user[0]
+
+    #User information
+    cursor.execute("SELECT email FROM Users WHERE email = ?",(email,))
+    user_data = cursor.fetchone()
+
+    #Bidder information
+    cursor.execute("SELECT first_name, last_name, age, major, home_address_id FROM Bidders WHERE email = ?",(email,))
+    bidder = cursor.fetchone()
+
+    #Address
+    address = None
+    if bidder:
+        add_id = bidder[4]
+        cursor.execute("SELECT street_num, street_name, zipcode FROM Address WHERE address_id = ?",(addr_id,))
+        address = cursor.fetchone()
+
+    #Card
+    cursor.execute("SELECT credit_card_num, card_type, expire_month, expire_year, security_code FROM Credit_Cards WHERE Owner_email = ?",(email,))
+    cards =cursor.fetchall()
+
+    #Seller information
+    cursor.execute("SELECT balance, bank_routing_number, bank_account_number FROM Sellers WHERE email = ?",(email,))
+    seller = cursor.fetchone()
+
+    connect.close()
+
+    #Return
+    return{
+        "email":email,
+        "bidder":{
+            "first_name": bidder[0] if bidder else None,
+            "last_name": bidder[1] if bidder else None,
+            "age": bidder[2] if bidder else None,
+            "major": bidder[3] if bidder else None,
+        },
+        "address":{
+            "street_num": address[0] if address else None,
+            "street_name": address[1] if address else None,
+            "zipcode": address[2] if address else None,
+        } if address else None,
+        "credit_card":[
+            {
+                "number": mask_card(c[0]),
+                "type": c[1],
+                "exp_month": c[2],
+                "exp_year": c[3],
+                "cvv": mask_cvv(c[4]),
+                "full_number": c[0]
+            } for c in cards
+        ],
+        "seller":{
+            "balance": seller[0] if seller else None,
+            "bank_routing": seller[1] if seller else None,
+            "bank_account": seller[2] if seller else None,
+        } if seller else None
+    }
+
+
+@app.route('/api/add-card', methods=['POST'])
+# Add card information
+def add_card():
+    token = request.headers.get('Authorization')
+    data = request.json
+
+    connect = sql.connect("database.db")
+    cursor = connect.cursor()
+    cursor.execute("SELECT email FROM Tokens WHERE token = ?", (token,))
+    user = cursor.fetchone()
+    if not user:
+        return Response(json.dumps({"error": "Unauthorized"}), 400)
+    email = user[0]
+
+    try:
+        cursor.execute(
+            "INSERT INTO Credit_Cards (credit_card_num, card_type, expire_month, expire_year, security_code, Owner_email)VALUES(?,?,?,?,?,?)",
+            (
+                data['number'],
+                data['type'],
+                data['exp_month'],
+                data['exp_year'],
+                data['cvv'],
+                email
+            ))
+        connect.commit()
+        connect.clone()
+        return {"message": "Card added successfully"}
+    except:
+        connect.close()
+        return Response(json.dumps({"error": "Failed"}), 500)
+
+
+@app.route('/api/remove-card', methods=['POST'])
+# Remove card information
+def remove_card():
+    token = request.headers.get('Authorization')
+    data = request.json
+
+    connect = sql.connect("database.db")
+    cursor = connect.cursor()
+    cursor.execute("SELECT email FROM Tokens WHERE token = ?", (token,))
+    user = cursor.fetchone()
+    if not user:
+        return Response(json.dumps({"error": "Unauthorized"}), 400)
+    email = user[0]
+
+    cursor.execute(
+        "DELETE FROM Credit_Cards WHERE Owner_email = ? AND credit_card_num = ?", (email, data['number']))
+    connect.commit()
+    connect.clone()
+    return {"message": "Card removed successfully"}
+
+
+@app.route('/api/request-password-change', methods=['POST'])
+# Seller request password change
+def request_password_change():
+    token = request.headers.get('Authorization')
+    connect = sql.connect("database.db")
+    cursor = connect.cursor()
+    cursor.execute("SELECT email FROM Tokens WHERE token = ?", (token,))
+    user = cursor.fetchone()
+    if not user:
+        return Response(json.dumps({"error": "Unauthorized"}), 400)
+    email = user[0]
+
+    try:
+        cursor.execute("INSERT INTO Requests(user_email, request_type, status) VALUES(?,'Password Change','Pending')",
+                       (email,))
+        connect.commit()
+        connect.close()
+        return {"message": "Request sent to helpdesk"}
+    except:
+        connect.close()
+        return Response(json.dumps({"error": "Failed"}), 500)
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port="5000")
